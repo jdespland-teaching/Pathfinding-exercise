f937bfca70244e75936a39d82addd7c1813bdeab
tiffhc <46658494+tiffhc@users.noreply.github.com>
2020-02-19 11:19:23 -0500

    key change
    Changed O to U

--- Assets/animate.cs
+++ Assets/animate.cs
@@ -17,7 +17,7 @@ public class animate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.O))
+        if (Input.GetKeyDown(KeyCode.U))
         {
             this.GetComponent<Animator>().SetBool("gethit", true);
         }
