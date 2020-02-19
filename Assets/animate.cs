b87b6ca281eaff8929d42422b91936983b4af3e6
jdespland-teaching <46430973+jdespland-teaching@users.noreply.github.com>
2020-02-19 10:48:08 -0500

    Initial commit

---
+++ Assets/animate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class animate : MonoBehaviour
+{
+
+    public GameObject destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<NavMeshAgent>().SetDestination(destination.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            this.GetComponent<Animator>().SetBool("gethit", true);
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            this.GetComponent<Animator>().SetBool("gethit", false);
+        }
+    }
+}
